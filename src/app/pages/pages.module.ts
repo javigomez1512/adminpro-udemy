a1746c76769b6c1a623fbea2618e2fb028c4c43d
Francisco Javier Gómez Rodríguez <javier.gomez@admiral-eu.com>
2020-06-18 12:03:10 +0200

    Input, output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
+import { PAGES_ROUTES } from './pages.routes';
 
 import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
+
+// ng-charts2
+import { ChartsModule } from 'ng2-charts';
 
 import { PagesComponent } from './pages.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { PAGES_ROUTES } from './pages.routes';
+
+
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
+
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+
 
 
 
@@ -16,7 +30,9 @@ import { PAGES_ROUTES } from './pages.routes';
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -25,7 +41,9 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule { }
